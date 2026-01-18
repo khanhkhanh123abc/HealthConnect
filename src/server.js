c36b6ffcc2a3b9973e460933d4cfb799efe6f0c7
khanhkhanh123abc <trandangkhanh200315@gmail.com>
2026-01-18 21:06:25 +0700

    Initial commit - HealthConnect project

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import configViewEngine from './config/viewEngine.js';
+import initWebRoutes from './route/web.js';
+import connectDB from './config/connectDB.js';
+import 'dotenv/config';
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+configViewEngine(app);
+initWebRoutes(app);
+
+connectDB();
+
+let port = process.env.PORT || 6969;
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
+
+//# sourceMappingURL=server.js.map
